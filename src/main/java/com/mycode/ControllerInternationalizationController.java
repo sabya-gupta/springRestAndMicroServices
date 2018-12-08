a8db00ce4ceda9a7e09bc265eaf130c7880f5d59
Sabyasachi.Gu <sabya.gupta@yahoo.com>
2018-12-08 13:22:10 +0530

    I18N

---
+++ src/main/java/com/mycode/ControllerInternationalizationController.java
@@ -0,0 +1,25 @@
+package com.mycode;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/i18n")
+public class ControllerInternationalizationController {
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	
+	@GetMapping("/greet")
+	public String greet(@RequestHeader(name="Lang-Of_Our_Application", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+	
+}
